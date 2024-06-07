8c4dc83b0880e265fbcc5d5563bfac46ce151100
dito rafi <diabdusalamrafi@gmail.com>
2024-06-07 15:50:08 +0700

    feat: landing page

--- apps/web/src/app/(root)/page.tsx
+++ apps/web/src/app/(root)/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import HeroPage from './components/Hero';
+import MostPopuler from './components/MostPopuler';
+import IklanPage from './components/Iklan';
+import CategoryPage from './components/Category';
+import ProductsPage from './components/Products';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 const page = () => {
   return (
-    <div>ini user</div>
-  )
-}
+    <>
+      <div className="w-full">
+        <Card>
+          <CardHeader>
+            <CardTitle>Test</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Test</p>
+          </CardContent>
+        </Card>
+      </div>
+      {/* <section className="flex  flex-col gap-y-20">
+       <HeroPage />
+       <div>ini most populer</div> 
+       <MostPopuler />
+       <IklanPage />
+       <CategoryPage />
+       <IklanPage />
+       <ProductsPage />
+       </section>
+      */}
+    </>
+  );
+};
 
-export default page
+export default page;
---
+++ apps/web/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <>
+      <div
+        className={`flex justify-between py-10 px-8 font-bold text-white shadow border-b-4 border-[#D9D9D9] `}
+      ></div>
+    </>
+  );
+}
