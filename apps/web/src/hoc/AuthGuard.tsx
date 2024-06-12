5416c0d58cd2353e802552022ad5577f3d74c1f8
SDHZed <sadamhack@gmail.com>
2024-06-12 23:55:51 +0700

    chore: add redux persist

---
+++ apps/web/src/hoc/AuthGuard.tsx
@@ -0,0 +1,4 @@
+'use client';
+
+import { useAppSelector } from '@/redux/hooks';
+import { redirect } from 'next/navigation';
--- apps/web/src/redux/store.ts
+++ apps/web/src/redux/store.ts
@@ -1,13 +1,60 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer  from "@/redux/slicers/userSlice"
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
+import userReducer from './slicers/userSlice';
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
+const createNoopStorage = () => {
+  return {
+    getItem() {
+      return Promise.resolve(null);
+    },
+    setItem(_key: string, value: number) {
+      return Promise.resolve(value);
+    },
+    removeItem() {
+      return Promise.resolve();
+    },
+  };
+};
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {user: UsersState}
-export type AppDispatch = typeof store.dispatch
+const storage =
+  typeof window !== 'undefined'
+    ? createWebStorage('local')
+    : createNoopStorage();
+
+const persistConfig = {
+  key: 'shoes',
+  storage,
+  timeout: 2000,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const makeConfiguredStore = () =>
+  configureStore({
+    reducer: rootReducer,
+  });
+
+export const makeStore = () => {
+  const isServer = typeof window === 'undefined';
+  if (isServer) {
+    return makeConfiguredStore();
+  } else {
+    const persistedReducer = persistReducer(persistConfig, rootReducer);
+    let store = configureStore({
+      reducer: persistedReducer,
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: false,
+        }),
+    });
+    (store as any).__persistor = persistStore(store);
+    return store;
+  }
+};
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
