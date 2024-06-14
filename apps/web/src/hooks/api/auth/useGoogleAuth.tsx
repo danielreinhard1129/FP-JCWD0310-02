460e0142a9cde26e8bdf481acaf08974cc51e3c0
dito rafi <diabdusalamrafi@gmail.com>
2024-06-14 08:50:26 +0700

    feat: login and register manual

--- apps/web/src/hooks/api/auth/useGoogleAuth.tsx
+++ apps/web/src/hooks/api/auth/useGoogleAuth.tsx
@@ -10,7 +10,7 @@ const useGoogleAuth = () => {
   const googleLogin = useGoogleLogin({
     onSuccess: async ({ code }) => {
       try {
-        const response = await axiosInstance.post('/auth/google', { code });
+        const response = await axiosInstance.post('/api/auth/google', { code });
 
         const { data } = response;
 
@@ -26,39 +26,3 @@ const useGoogleAuth = () => {
 };
 
 export default useGoogleAuth;
-
-// // import { useEffect } from 'react';
-// // import useAxios from '@/hooks/useAxios';
-// // import { useGoogleLogin } from '@react-oauth/google';
-
-// // const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-
-// // const useGoogleAuth = () => {
-// //   const { axiosInstance } = useAxios();
-
-// //   const googleLogin = useGoogleLogin({
-// //     // clientId: googleClientId,
-// //     onSuccess: async ({ code }) => {
-// //       try {
-// //         const response = await axiosInstance.post('/auth/google', { code });
-
-// //         const { data } = response;
-
-// //         console.log('Login successful, server response:', data);
-// //       } catch (error) {
-// //         // console.error('Error during Google login:', error.response.data);
-// //       }
-// //     },
-// //     flow: 'auth-code',
-// //   });
-
-// //   useEffect(() => {
-// //     if (googleClientId === undefined) {
-// //       console.error('Google Client ID is not defined.');
-// //     }
-// //   }, [googleClientId]);
-
-// //   return { googleLogin };
-// // };
-
-// // export default useGoogleAuth;
